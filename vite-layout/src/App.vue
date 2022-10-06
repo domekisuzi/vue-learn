ba3161f08b83b90b27ebac107a903409e4ef5ba5
domekisuzi <domekisuzi@163.com>
2022-10-06 23:25:49 +0800

    学一半

--- vite-layout/src/App.vue
+++ vite-layout/src/App.vue
@@ -1,67 +1,91 @@
 <script setup>
-  // import {ref} from "vue";
-  // /*
-  // 数据
-  //  */
-  // let queryInput = ref("")
-  // let tableData = ref([
-  //   {
-  //     date: '2016-05-03',
-  //     name: 'Tom',
-  //     address: 'No. 189, Grove St, Los Angeles',
-  //   },
-  //   {
-  //     date: '2016-05-02',
-  //     name: 'Tom',
-  //     address: 'No. 189, Grove St, Los Angeles',
-  //   },
-  //   {
-  //     date: '2016-05-04',
-  //     name: 'Tom',
-  //     address: 'No. 189, Grove St, Los Angeles',
-  //   },
-  //   {
-  //     date: '2016-05-01',
-  //     name: 'Tom',
-  //     address: 'No. 189, Grove St, Los Angeles',
-  //   },
-  // ])
-  //
-  //
-  // const handlerRowClick = () =>{
-  //   console.log('click')
-  // }
+// import {ref} from "vue";
+// /*
+// 数据
+//  */
+// let queryInput = ref("")
+// let tableData = ref([
+//   {
+//     date: '2016-05-03',
+//     name: 'Tom',
+//     address: 'No. 189, Grove St, Los Angeles',
+//   },
+//   {
+//     date: '2016-05-02',
+//     name: 'Tom',
+//     address: 'No. 189, Grove St, Los Angeles',
+//   },
+//   {
+//     date: '2016-05-04',
+//     name: 'Tom',
+//     address: 'No. 189, Grove St, Los Angeles',
+//   },
+//   {
+//     date: '2016-05-01',
+//     name: 'Tom',
+//     address: 'No. 189, Grove St, Los Angeles',
+//   },
+// ])
+//
+//
+// const handlerRowClick = () =>{
+//   console.log('click')
+// }
 </script>
 
 <template>
+  <div class="common-layout">
+    <el-container class="wrapper">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 
-<!--  <div class="table-box">-->
-<!--&lt;!&ndash;    标题&ndash;&gt;-->
-<!--    <h2 class="title">最简单的 CRUD DEMO</h2>-->
-<!--    <div class="query-box">-->
+  <!--  <div class="table-box">-->
+  <!--&lt;!&ndash;    标题&ndash;&gt;-->
+  <!--    <h2 class="title">最简单的 CRUD DEMO</h2>-->
+  <!--    <div class="query-box">-->
 
-<!--      <el-input-->
-<!--          v-model="queryInput"-->
-<!--          :rows="1"-->
-<!--          type="input"-->
-<!--          placeholder="Please input"-->
-<!--      />-->
+  <!--      <el-input-->
+  <!--          v-model="queryInput"-->
+  <!--          :rows="1"-->
+  <!--          type="input"-->
+  <!--          placeholder="Please input"-->
+  <!--      />-->
 
-<!--      <el-button type="primary" @click="handlerRowClick">增加</el-button>-->
-<!--    </div>-->
-<!--&lt;!&ndash;    table&ndash;&gt;-->
-<!--    <el-table :data="tableData" border style="width: 100%">-->
-<!--      <el-table-column prop="date" label="Date" width="180" />-->
-<!--      <el-table-column prop="name" label="Name" width="180" />-->
-<!--      <el-table-column prop="address" label="Address" />-->
-<!--    </el-table>-->
-<!--  </div>-->
+  <!--      <el-button type="primary" @click="handlerRowClick">增加</el-button>-->
+  <!--    </div>-->
+  <!--&lt;!&ndash;    table&ndash;&gt;-->
+  <!--    <el-table :data="tableData" border style="width: 100%">-->
+  <!--      <el-table-column prop="date" label="Date" width="180" />-->
+  <!--      <el-table-column prop="name" label="Name" width="180" />-->
+  <!--      <el-table-column prop="address" label="Address" />-->
+  <!--    </el-table>-->
+  <!--  </div>-->
 
 </template>
 
 
 <style scoped>
 
+.el-aside{
+  background-color: #747bff;
+}
+.el-header{
+  background-color: red;
+}
+.el-main{
+  background-color: orange;
+}
+.el-footer{
+  background-color: green;
+}
 /*.table-box{*/
 /*  width: 800px;*/
 /*  position: absolute;*/
