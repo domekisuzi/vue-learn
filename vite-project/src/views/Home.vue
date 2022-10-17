f0bcbe87f6cb58c859947ea0e569212e65c9820a
domekisuzi <domekisuzi@163.com>
2022-10-17 13:54:53 +0800

    完成了大部分首页

--- vite-project/src/views/Home.vue
+++ vite-project/src/views/Home.vue
@@ -2,10 +2,14 @@
 
 <template>
   <Header></Header>
+  <NavSwiper></NavSwiper>
 </template>
 
-<script>
+<script setup>
 import Header from '@/components/common/Header.vue'
+import NavSwiper from  '@/components/home/NavSwiper.vue'
+
+
 </script>
 
 <style scoped>
