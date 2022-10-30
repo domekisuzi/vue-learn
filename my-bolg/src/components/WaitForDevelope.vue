cf8adeed8885253f8899aa687ba0f0fcad9fe89f
domekisuzi <domekisuzi@163.com>
2022-10-30 22:53:06 +0800

    界面设计之前一天

---
+++ my-bolg/src/components/WaitForDevelope.vue
@@ -0,0 +1,13 @@
+<!-- made by domekisuzi @2022/10/30 -->
+
+<template>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-bolg/src/views/MyLife/index.vue
@@ -0,0 +1,41 @@
+<!-- made by domekisuzi @2022/10/30 -->
+
+<template>
+  <div class="container">
+    <div class="header">
+      <Header></Header>
+    </div>
+    <div class="main">
+
+    </div>
+    <div class="footer">
+      <Footer></Footer>
+    </div>
+
+  </div>
+
+</template>
+
+<script setup>
+import Header from '../../components/Header.vue'
+
+import Footer from '../../components/Footer.vue'
+
+</script>
+
+<style scoped>
+.container{
+  background-image: url('../../assets/chainsaw.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height: 721.666px;
+}
+
+.header{
+  background: linear-gradient(to right, rgb(22, 22, 52) 1%, rgba(255, 255, 255, 0.65)50%, rgb(22, 22, 52) 100%) ;
+
+}
+
+</style>
